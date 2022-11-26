d88bff8a317ad9ae746b4321a771c1a2d04151bd
mdrlzy <mordeniusss@gmail.com>
2022-11-27 03:49:52 +0600

    EditCanvas implemented

---
+++ app/src/main/java/space/taran/arkretouch/presentation/drawing/EditCanvas.kt
@@ -0,0 +1,95 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
+package space.taran.arkretouch.presentation.drawing
+
+import android.graphics.PointF
+import android.view.MotionEvent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import space.taran.arkretouch.presentation.edit.EditViewModel
+
+@Composable
+fun EditCanvas(viewModel: EditViewModel) {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        viewModel.editManager.backgroundImage.value?.let { imageBitmap ->
+            drawImage(
+                imageBitmap,
+                topLeft = viewModel.editManager.calcImageOffset()
+            )
+        }
+    }
+    EditDrawCanvas(viewModel)
+}
+
+@Composable
+fun EditDrawCanvas(viewModel: EditViewModel) {
+    val editManager = viewModel.editManager
+    var path = Path()
+    val currentPoint = PointF(0f, 0f)
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            // Eraser leaves black line instead of erasing without this hack, it uses BlendMode.SrcOut
+            // https://stackoverflow.com/questions/65653560/jetpack-compose-applying-porterduffmode-to-image
+            // Provide a slight opacity to for compositing into an
+            // offscreen buffer to ensure blend modes are applied to empty pixel information
+            // By default any alpha != 1.0f will use a compositing layer by default
+            .graphicsLayer(alpha = 0.99f)
+            .pointerInteropFilter { event ->
+                val motionTouchEventX = event.x
+                val motionTouchEventY = event.y
+
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        path.reset()
+                        path.moveTo(motionTouchEventX, motionTouchEventY)
+                        currentPoint.x = motionTouchEventX
+                        currentPoint.y = motionTouchEventY
+                        editManager.addDrawPath(path)
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        path.quadraticBezierTo(
+                            currentPoint.x,
+                            currentPoint.y,
+                            (motionTouchEventX + currentPoint.x) / 2,
+                            (motionTouchEventY + currentPoint.y) / 2
+                        )
+                        currentPoint.x = motionTouchEventX
+                        currentPoint.y = motionTouchEventY
+                    }
+                    MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP -> {
+                        // draw a dot
+                        if (motionTouchEventX == currentPoint.x &&
+                            motionTouchEventY == currentPoint.y
+                        ) {
+                            path.lineTo(currentPoint.x, currentPoint.y)
+                        }
+
+                        editManager.clearRedoPath()
+                        editManager.updateRevised()
+                        path = Path()
+                    }
+                    else -> false
+                }
+                editManager.invalidatorTick.value++
+                true
+            }
+    ) {
+        // force recomposition on invalidatorTick change
+        editManager.invalidatorTick.value
+
+        drawIntoCanvas { canvas ->
+            editManager.drawPaths.forEach {
+                canvas.drawPath(it.path, it.paint)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/space/taran/arkretouch/presentation/drawing/EditManager.kt
@@ -0,0 +1,152 @@
+package space.taran.arkretouch.presentation.drawing
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.PaintingStyle
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.unit.IntSize
+import java.util.Stack
+
+class EditManager {
+    private val drawPaint: MutableState<Paint> = mutableStateOf(defaultPaint())
+    private val erasePaint: Paint = Paint().apply {
+        shader = null
+        color = Color.Transparent
+        style = PaintingStyle.Stroke
+        blendMode = BlendMode.SrcOut
+    }
+    val currentPaint: Paint
+        get() = if (isEraseMode.value) {
+            erasePaint
+        } else {
+            drawPaint.value
+        }
+
+    val drawPaths = Stack<DrawPath>()
+    private val redoPaths = Stack<DrawPath>()
+
+    var backgroundImage = mutableStateOf<ImageBitmap?>(null)
+    var drawAreaSize = mutableStateOf(IntSize.Zero)
+
+    var invalidatorTick = mutableStateOf(0)
+
+    private val _currentPaintColor: MutableState<Color> =
+        mutableStateOf(drawPaint.value.color)
+    val currentPaintColor: State<Color> = _currentPaintColor
+
+    private val _isEraseMode: MutableState<Boolean> = mutableStateOf(false)
+    val isEraseMode: State<Boolean> = _isEraseMode
+
+    private val _canUndo: MutableState<Boolean> = mutableStateOf(false)
+    val canUndo: State<Boolean> = _canUndo
+
+    private val _canRedo: MutableState<Boolean> = mutableStateOf(false)
+    val canRedo: State<Boolean> = _canRedo
+
+    internal fun clearRedoPath() {
+        redoPaths.clear()
+    }
+
+    fun updateRevised() {
+        _canUndo.value = drawPaths.isNotEmpty()
+        _canRedo.value = redoPaths.isNotEmpty()
+    }
+
+    fun undo() {
+        if (canUndo.value) {
+            redoPaths.push(drawPaths.pop())
+            invalidatorTick.value++
+            updateRevised()
+        }
+    }
+
+    fun redo() {
+        if (canRedo.value) {
+            drawPaths.push(redoPaths.pop())
+            invalidatorTick.value++
+            updateRevised()
+        }
+    }
+
+    fun addDrawPath(path: Path) {
+        drawPaths.add(
+            DrawPath(
+                path,
+                currentPaint.copy().apply {
+                    strokeWidth = drawPaint.value.strokeWidth
+                }
+            )
+        )
+    }
+
+    fun setPaintColor(color: Color) {
+        drawPaint.value.color = color
+        _currentPaintColor.value = color
+    }
+
+    fun clearPaths() {
+        drawPaths.clear()
+        redoPaths.clear()
+        invalidatorTick.value++
+        updateRevised()
+    }
+
+    fun toggleEraseMode() {
+        _isEraseMode.value = !isEraseMode.value
+    }
+
+    fun setPaintStrokeWidth(strokeWidth: Float) {
+        drawPaint.value.strokeWidth = strokeWidth
+    }
+
+    fun calcImageOffset(): Offset {
+        val drawArea = drawAreaSize.value
+        val bitmap = backgroundImage.value!!
+        val xOffset = (drawArea.width - bitmap.width) / 2f
+        val yOffset = (drawArea.height - bitmap.height) / 2f
+        return Offset(xOffset, yOffset)
+    }
+}
+
+class DrawPath(
+    val path: Path,
+    val paint: Paint
+)
+
+fun Paint.copy(): Paint {
+    val from = this
+    return Paint().apply {
+        alpha = from.alpha
+        isAntiAlias = from.isAntiAlias
+        color = from.color
+        blendMode = from.blendMode
+        style = from.style
+        strokeWidth = from.strokeWidth
+        strokeCap = from.strokeCap
+        strokeJoin = from.strokeJoin
+        strokeMiterLimit = from.strokeMiterLimit
+        filterQuality = from.filterQuality
+        shader = from.shader
+        colorFilter = from.colorFilter
+        pathEffect = from.pathEffect
+    }
+}
+
+fun defaultPaint(): Paint {
+    return Paint().apply {
+        color = Color.White
+        strokeWidth = 14f
+        isAntiAlias = true
+        style = PaintingStyle.Stroke
+        strokeJoin = StrokeJoin.Round
+        strokeCap = StrokeCap.Round
+    }
+}
