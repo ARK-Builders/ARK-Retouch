da86e8224d05830da737bdedd130d07072a31b63
ShubertMunthali <munthalimartinz@gmail.com>
2023-07-07 11:48:50 +0200

    Ask user to overwrite

--- app/src/main/java/space/taran/arkretouch/presentation/edit/SavePathDialog.kt
+++ app/src/main/java/space/taran/arkretouch/presentation/edit/SavePathDialog.kt
@@ -3,7 +3,6 @@ package space.taran.arkretouch.presentation.edit
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,6 +10,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Checkbox
@@ -42,7 +42,12 @@ import space.taran.arkretouch.R
 import space.taran.arkretouch.presentation.utils.findNotExistCopyName
 import kotlin.io.path.name
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.key
+import androidx.compose.ui.ExperimentalComposeUiApi
+import java.nio.file.Files
+import kotlin.streams.toList
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SavePathDialog(
     initialImagePath: Path?,
@@ -51,6 +56,8 @@ fun SavePathDialog(
     onPositiveClick: (Path) -> Unit
 ) {
     var currentPath by remember { mutableStateOf(initialImagePath?.parent) }
+    var imagePath by remember { mutableStateOf(initialImagePath) }
+    val showOverwriteCheckbox = remember { mutableStateOf(initialImagePath != null) }
     var overwriteOriginalPath by remember { mutableStateOf(false) }
     var name by remember {
         mutableStateOf(
@@ -59,93 +66,121 @@ fun SavePathDialog(
             } ?: "image.png"
         )
     }
+
     val lifecycleOwner = LocalLifecycleOwner.current
+
     LaunchedEffect(overwriteOriginalPath) {
         if (overwriteOriginalPath) {
-            currentPath = initialImagePath!!.parent
-            name = initialImagePath.name
+            imagePath?.let {
+                currentPath = it.parent
+                name = it.name
+            }
+            return@LaunchedEffect
+        }
+        imagePath?.let {
+            name = it.parent.findNotExistCopyName(it.fileName).name
         }
     }
 
-    Dialog(onDismissRequest = onDismissClick) {
-        Column(
-            Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .background(Color.White, RoundedCornerShape(5))
-                .padding(5.dp)
-        ) {
-            Text(
-                modifier = Modifier.padding(5.dp),
-                text = stringResource(R.string.location),
-                fontSize = 18.sp
-            )
-            TextButton(
-                onClick = {
-                    ArkFilePickerFragment
-                        .newInstance(
-                            folderFilePickerConfig(currentPath)
-                        )
-                        .show(fragmentManager, null)
-                    fragmentManager.onArkPathPicked(lifecycleOwner) {
-                        currentPath = it
-                    }
-                }
+    key(showOverwriteCheckbox.value) {
+        Dialog(onDismissRequest = onDismissClick) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .background(Color.White, RoundedCornerShape(5))
+                    .padding(5.dp)
             ) {
                 Text(
-                    text = currentPath?.toString()
-                        ?: stringResource(R.string.pick_folder)
+                    modifier = Modifier.padding(5.dp),
+                    text = stringResource(R.string.location),
+                    fontSize = 18.sp
                 )
-            }
-            OutlinedTextField(
-                modifier = Modifier.padding(5.dp),
-                value = name.toString(),
-                onValueChange = {
-                    name = it
-                },
-                label = { Text(text = stringResource(R.string.name)) },
-                singleLine = true
-            )
-
-            initialImagePath?.let {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(5))
-                        .clickable {
-                            overwriteOriginalPath = !overwriteOriginalPath
-                        }
-                        .padding(5.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Checkbox(
-                        checked = overwriteOriginalPath,
-                        onCheckedChange = {
-                            overwriteOriginalPath = !overwriteOriginalPath
-                        }
-                    )
-                    Text(text = stringResource(R.string.overwrite_original_file))
-                }
-            }
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.End
-            ) {
                 TextButton(
-                    modifier = Modifier.padding(5.dp),
-                    onClick = onDismissClick
-                ) {
-                    Text(text = stringResource(R.string.cancel))
-                }
-                Button(
-                    modifier = Modifier.padding(5.dp),
                     onClick = {
-                        if (currentPath != null && name != null)
-                            onPositiveClick(currentPath!!.resolve(name))
+                        ArkFilePickerFragment
+                            .newInstance(
+                                folderFilePickerConfig(currentPath)
+                            )
+                            .show(fragmentManager, null)
+                        fragmentManager.onArkPathPicked(lifecycleOwner) { path ->
+                            currentPath = path
+                            currentPath?.let {
+                                imagePath = it.resolve(name)
+                                showOverwriteCheckbox.value = Files.list(it).toList()
+                                    .contains(imagePath)
+                                if (showOverwriteCheckbox.value) {
+                                    name = it.findNotExistCopyName(
+                                        imagePath?.fileName!!
+                                    ).name
+                                }
+                            }
+                        }
                     }
                 ) {
-                    Text(text = stringResource(R.string.ok))
+                    Text(
+                        text = currentPath?.toString()
+                            ?: stringResource(R.string.pick_folder)
+                    )
+                }
+                OutlinedTextField(
+                    modifier = Modifier.padding(5.dp),
+                    value = name,
+                    onValueChange = {
+                        name = it
+                        currentPath?.let { path ->
+                            imagePath = path.resolve(name)
+                            showOverwriteCheckbox.value = Files.list(path).toList()
+                                .contains(imagePath)
+                            if (showOverwriteCheckbox.value) {
+                                name = path.findNotExistCopyName(
+                                    imagePath?.fileName!!
+                                ).name
+                            }
+                        }
+                    },
+                    label = { Text(text = stringResource(R.string.name)) },
+                    singleLine = true
+                )
+                if (showOverwriteCheckbox.value) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(5))
+                            .clickable {
+                                overwriteOriginalPath = !overwriteOriginalPath
+                            }
+                            .padding(5.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Checkbox(
+                            checked = overwriteOriginalPath,
+                            onCheckedChange = {
+                                overwriteOriginalPath = !overwriteOriginalPath
+                            }
+                        )
+                        Text(text = stringResource(R.string.overwrite_original_file))
+                    }
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    TextButton(
+                        modifier = Modifier.padding(5.dp),
+                        onClick = onDismissClick
+                    ) {
+                        Text(text = stringResource(R.string.cancel))
+                    }
+                    Button(
+                        modifier = Modifier.padding(5.dp),
+                        onClick = {
+                            if (currentPath != null && name != null)
+                                onPositiveClick(currentPath!!.resolve(name))
+                        }
+                    ) {
+                        Text(text = stringResource(R.string.ok))
+                    }
                 }
             }
         }
